486bc90a4e62a0adb16905aa6eadd645b41876e9
srpsmruti <srp.smruti@gmail.com>
2023-06-18 16:17:30 +0530

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -4,18 +4,28 @@
 THRESHOLD_CPU=80
 THRESHOLD_MEM=80
 THRESHOLD_DISK=80
-EMAIL="srp.smruti@gmail.com"
-BITNAMI_STATUS_SCRIPT="/opt/bitnami/ctlscript.sh"
-DB_HOSTNAME="127.0.0.1"
-DB_USERNAME="bn_opencart"
-DB_PASSWORD="3ebdebdfa7be3af2baac1121bd58a5b3daf40b740ad47e433f2ae12516bb7c89"
-DB_DATABASE="bitnami_opencart"
+#EMAIL="babu@testleaf.com"
+#BITNAMI_STATUS_SCRIPT="/opt/bitnami/ctlscript.sh"
+#DB_HOSTNAME="127.0.0.1"
+#DB_USERNAME="bn_opencart"
+#DB_PASSWORD="fe390080a5573e5bbfb0ca29b926fa5529ea60020aec11c5979354f3c128f7b4"
+#DB_DATABASE="bitnami_opencart"
 DB_PORT=3306
 
 # Track if any checks fail
 checks_passed=true
 
 # Check CPU usage
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+
+echo "CPU Usage: $cpu_usage"
+echo "CPU usage is higher than $THRESHOLD_CPU%, it's $cpu_usage%"
+
+#if (( $(echo "$cpu_usage > $THRESHOLD_CPU" |bc -l) )); then
+#  echo "CPU usage is higher than $THRESHOLD_CPU%, it's $cpu_usage%" | \
+#  mail -s "High CPU usage alert" $EMAIL
+#  checks_passed=false
+#fi
 
 # Check Memory usage
 mem_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
@@ -36,26 +46,26 @@ echo "Disk Usage: $disk_usage"
 #if (( $(echo "$disk_usage > $THRESHOLD_DISK" |bc -l) )); then
 #  echo "Disk usage is higher than $THRESHOLD_DISK%, it's $disk_usage%" | \
 #  mail -s "High Disk usage alert" $EMAIL
-#  checks_passed=false
+ # checks_passed=false
 #fi
 
 # Check status of Bitnami services
-services_status=$(sudo $BITNAMI_STATUS_SCRIPT status)
+#services_status=$(sudo $BITNAMI_STATUS_SCRIPT status)
 
-echo "Service Status: $services_status"
+#echo "Service Status: $services_status"
 
-if echo $services_status | grep -q "not running"; then
-  echo "One or more Bitnami services are not running: $services_status" | \
-  mail -s "Bitnami service status alert" $EMAIL
-  checks_passed=false
-fi
+#if echo $services_status | grep -q "not running"; then
+  #echo "One or more Bitnami services are not running: $services_status" | \
+  #mail -s "Bitnami service status alert" $EMAIL
+ # checks_passed=false
+#fi
 
 # Check MySQL status
-if ! mysqladmin -h $DB_HOSTNAME -P $DB_PORT -u $DB_USERNAME -p$DB_PASSWORD status > /dev/null; then
-  echo "MySQL is not running" | \
-  mail -s "MySQL is down alert" $EMAIL
-  checks_passed=false
-fi
+#if ! mysqladmin -h $DB_HOSTNAME -P $DB_PORT -u $DB_USERNAME -p$DB_PASSWORD status > /dev/null; then
+ # echo "MySQL is not running" | \
+  #mail -s "MySQL is down alert" $EMAIL
+  #checks_passed=false
+#fi
 
 # If all checks passed, run the tests
 if $checks_passed; then
